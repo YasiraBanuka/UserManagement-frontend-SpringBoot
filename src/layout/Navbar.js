070811e44ba0f0cbe08277346441f32ea1130401
IT21378270 <it21378270@my.sliit.lk>
2023-07-05 19:49:11 +0530

    frontend-completed

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
@@ -6,7 +7,7 @@ export default function Navbar() {
 
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">User Management Application</a>
+            <Link className="navbar-brand" to={"/"}>User Management Application</Link>
             <button 
                 className="navbar-toggler" 
                 type="button" 
@@ -17,7 +18,7 @@ export default function Navbar() {
             >
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <button className="btn btn-outline-light">Add User</button>
+            <Link className="btn btn-outline-light" to={"/adduser"}>Add User</Link>
         </div>
         </nav>
       
